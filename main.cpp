de33aced8cb9d1bfe65d2bd148df828adb851568
Kael-S <kaelsuggs@gmail.com>
2018-03-29 09:39:45 -0500

    Added where the piece would be moved & validMove functionallity
    mostly commented out right now, can be ready when legalMove method is working.

--- main.cpp
+++ main.cpp
@@ -8,9 +8,24 @@ using namespace std;
 
 int main() {
 	string input;
+	string movedTo;
 	Board b = Board();
+	int validMove = 0;
 	cout << "Please enter the location of the piece to be moved: " << endl;
 	cin >> input;
+	cout << endl << "Please enter where you would like to move the piece:" << endl;
+	//test for if the move was valid
+	/* do cin >> movedTo;
+	while (validMove = 0;) {
+		if () {
+			validMove = 1;
+		}
+		else {
+			cout << "Invalid Move. Please Try Again: " << endl;
+			cin >> movedTo;
+		}
+	}
+	*/
 	cout << "Hello world!" << endl;
 	for (int i=0; i<8; i++) {
 		for(int j=0; j<8; j++) {
